487bd48d345145a2637d540a0333d2c6d23baac2
ridhoemgl <ridhoemgl@gmail.com>
2026-08-24 23:58:08 +0900

    update Notify

--- Pages/Admin/Master/Questions.cshtml.cs
+++ Pages/Admin/Master/Questions.cshtml.cs
@@ -74,7 +74,7 @@ public class QuestionsModel : AdminPageModelBase
 
         _context.Questions.Remove(entity);
         await _context.SaveChangesAsync(cancellationToken);
-        Notify($"Pertanyaan \"{entity.QuestionText  }\" berhasil dihapus.");
+        Notify($"Pertanyaan \"{entity.CreatedByEmployee  }\" berhasil dihapus.");
 
         return RedirectToList();
     }
