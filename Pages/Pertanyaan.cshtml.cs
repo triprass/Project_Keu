1788ee6996fb23ebd4a1a59c2e4dff6ef6c4974f
ridhoemgl <ridhoemgl@gmail.com>
2026-08-24 22:13:59 +0900

    update func2

--- Pages/Pertanyaan.cshtml.cs
+++ Pages/Pertanyaan.cshtml.cs
@@ -103,7 +103,6 @@ public class PertanyaanModel : PageModel
             }
         }
 
-        // Kembalikan jumlah total perangkat aktif yang unik
         return new JsonResult(new { count = ActiveUsers.Count });
     }
 }
