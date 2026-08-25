c7b9fc687e8f6317d09250ccaae6d15ba979d3e2
ridhoemgl <ridhoemgl@gmail.com>
2026-08-26 01:30:38 +0900

    update

--- Services/Notifications/AzureAcsService.cs
+++ Services/Notifications/AzureAcsService.cs
@@ -6,7 +6,7 @@ namespace PilarKeuangan.Services.Notifications
     {
         private readonly NotificationMessagesClient _messagesClient;
 
-        private const string ConnectionString = "endpoint=https://acs-pilar-notification-service.indonesia.communication.azure.com/;accesskey=KODE_ACCESS_KEY_AZURE_ANDA";
+        private const string ConnectionString = "endpoint=https://acs-pilar-notification-service.indonesia.communication.azure.com/;accesskey=KODE_ACCESS_KEY_AZURE_ANDA" ;
         private static readonly Guid ChannelRegistrationId = Guid.Parse("8d5b2ee9-1581-4917-b73f-22bf44abb2af");
 
         public AzureAcsService()
