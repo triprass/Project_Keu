5af6d9b145e45a634b2a553dabfea942147354df
ridhoemgl <ridhoemgl@gmail.com>
2026-08-27 18:18:23 +0900

    revert

--- Pages/DetailPertanyaan.cshtml.cs
+++ Pages/DetailPertanyaan.cshtml.cs
@@ -43,7 +43,7 @@ public class DetailPertanyaanModel : PageModel
     /// <summary>Kategori pertanyaan, dipakai tombol "Kembali" agar mendarat di daftar yang sama.</summary>
     public Guid? CategoryId => SelectedQuestion?.CategoryId;
 
-    public IActionResult OnPost()
+    public IActionResult OnGet()
     {
         // Halaman ini hanya bisa dibuka lewat POST dari daftar pertanyaan.
         // Akses langsung tanpa id ditampilkan sebagai halaman kosong.
