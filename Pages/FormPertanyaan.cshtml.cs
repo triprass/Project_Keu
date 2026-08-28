95a7ac7092df8f2b1fe9256f0859f29c5531e20d
ridhoemgl <ridhoemgl@gmail.com>
2026-08-28 09:30:50 +0900

    update script notif open

--- Pages/FormPertanyaan.cshtml.cs
+++ Pages/FormPertanyaan.cshtml.cs
@@ -162,10 +162,11 @@ public class FormPertanyaanModel : PageModel
             // 082111191354     Pak Mario
             // 083145710015     Aji
             // 081337645975     Dinu
-            string targetPhonePIC = "082111191354,083145710015,081337645975";
+            //string targetPhonePIC = "082111191354,083145710015,081337645975";
+            string targetPhonePIC = "082111191354";
 
-            string messageBody2 = _fonnteService.BuildTicketTemplate2(ticketNo, senderName, nip, unitKerja, kategoriPertanyaan);
-            await _fonnteService.SendWhatsAppMessageAsync(targetPhonePIC, messageBody2);
+            messageBody = _fonnteService.BuildTicketTemplate2(ticketNo, senderName, nip, unitKerja, kategoriPertanyaan);
+            await _fonnteService.SendWhatsAppMessageAsync(targetPhonePIC, messageBody);
 
             try
             {
