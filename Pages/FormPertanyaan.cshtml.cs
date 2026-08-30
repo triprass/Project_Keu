6898960ef3a12f5221961cfce50184c9a9afe42b
ridhoemgl <ridhoemgl@gmail.com>
2026-08-30 23:35:38 +0900

    update comment

--- Pages/FormPertanyaan.cshtml.cs
+++ Pages/FormPertanyaan.cshtml.cs
@@ -170,7 +170,7 @@ public class FormPertanyaanModel : PageModel
             // [SCRIPT PUSH NOTIFICATION FONNTE]
             // Kirim Pesan Notifikasi ke Pembuat Pertanyaan [Open]
             string messageBody = _fonnteService.BuildTicketTemplate1(ticketNo, senderName, nip, unitKerja, kategoriPertanyaan);
-            await _fonnteService.SendWhatsAppMessageAsync(targetPhone, messageBody);
+            //await _fonnteService.SendWhatsAppMessageAsync(targetPhone, messageBody);
 
             // Kirim Pesan Notifikasi ke PIC Keuangan [Open]
             //messageBody = _fonnteService.BuildTicketTemplate2(ticketNo, senderName, nip, unitKerja, kategoriPertanyaan);
